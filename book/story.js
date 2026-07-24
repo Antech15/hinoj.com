c0ea91393079f020422fce7390e7bf231103e137
Andres Hinojosa <108111237+Antech15@users.noreply.github.com>
2026-07-23 23:14:57 -0400

    s

---
+++ book/story.js
@@ -0,0 +1,27 @@
+const pages = [
+  {
+    type: 'title',
+    title: 'My Story', // <-- your book title
+    author: 'Andres Hinojosa'
+  },
+  {
+    label: 'Chapter One',
+    title: 'The Beginning',
+    body: [
+      'The day is Monday. September 2nd, 2002 at Scripps Memorial Hospital in Encinitas, California.',
+      'On this day, I was born. I was told that I almost didn\'t make it, but I did. Apparently, I came out looking like Thanos baby, being kind of on the purple side from lack of oxygen.',
+      'I was a chubby baby, but that didn\'t seem to last long. There\'s not much to say about my very early years as I don\'t remember much. But I do remember very well playing outside with my sister often.'
+    ]
+  },
+  {
+    label: 'Chapter Two',
+    title: 'Preschool and Kindergarten',
+    body: [
+      'Growing up, I only remember ever living in Escondido. That\'s when I gained consciousness I guess. Anyways, I do remember going into preschool.',
+      'I don\'t entirely know what had caused this, but I am and always have been a shy person. I didn\'t make many friends, and I was completely clueless at times.',
+      'In preschool, I remember learning how to write my name, and simple sentences. I remember watching The Cat in the Hat, and making lots of arts and crafts.',
+      'On the note of being clueless, for some reason, every time I would go outside whether it be for the bathroom or sending a message from my teacher to another, I ended up being filled with anxiety and fear. I wouldn\'t know for sure if I was going to knock at the right door. So I ended up pacing around, and sometimes feel hopeless and start to wander around the school. I don\'t know why I was like this. And as a child, I wouldn\'t have sense of time. One day, I was out for so long that I got a stern talking to from my teacher. At the time, I thought she was being mean, but now I know that if you are in charge of keeping an eye on children for the whole day, you simply cannot afford to lose a child. She was probably just as scared as I was.',
+      'Another big event I remember was that I almost was diagnosed with autism. Because I was extremely shy back then, I was practivally nonverbal most of the time. I would only speak when spoken to, and even then I wouldn\'t say much. One day, the school told my mom that I\'d have to go to a place in San Diego to be tested if I didn\'t improve. During this time, my mom was a single mother, so spending money on a test, commuting by bus to San Diego and all wasn\'t in the cards for us. Though I couldn\'t grasp this situation, I could feel my mother being very worried and stressed. She asked me to try to speak more and explained that we were struggling. All I know is that ever since then, I started to talk more and while I didn\'t make lasting friends, I did occasinally talk to some people.'
+    ]
+  }
+];
